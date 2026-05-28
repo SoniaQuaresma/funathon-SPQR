1b056b407ba0105cfd52d86d1480ccb133862aa4
soniaquaresma <sonia.quaresma@ine.pt>
2026-05-28 14:47:15 +0000

    logging_try3

--- starting_point/main.py
+++ starting_point/main.py
@@ -1,9 +1,9 @@
 # %%
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor, HistGradientBoostingRegressor
+#from sklearn.model_selection import train_test_split
+#from sklearn.ensemble import RandomForestRegressor, HistGradientBoostingRegressor
 from preprocess import complete_pre_processing
 from log_mlflow import log_to_mlflow
-from pipeline import set_pipeline
+#from pipeline import set_pipeline
 from utils import setup_logging, set_seed
 
 logger = setup_logging()
